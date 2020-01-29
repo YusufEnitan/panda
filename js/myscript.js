f8cfa65eb4748691d4cfd68a7090de26fcf80c25
YusufEnitan <ennieyusuf@gmail.com>
2020-01-29 14:27:13 +0000

    My first commit

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+/*function clickMe(){
+  alert("Message sent");
+}
+document.getElementById("clickMe").onclick=clickMe();*/
+
+/*var age = 19 ;
+if(age<=18){
+  alert("too young");
+}
+else{
+  alert("old enough");
+}*/
